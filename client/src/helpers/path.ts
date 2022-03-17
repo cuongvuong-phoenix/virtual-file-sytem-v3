a6807cbba6317e2359965987153e27a96d4a6557
Cuong Vuong Chi <cuongvuong.phoenix@gmail.com>
2022-03-17 22:09:32 +0700

    feat(client/helpers): implement `encodePath` function

--- client/src/helpers/path.ts
+++ client/src/helpers/path.ts
@@ -1,3 +1,32 @@
 export function decodePath(pathArr: string[]) {
   return `/${pathArr.join('/')}`;
 }
+
+export function encodePath(path: string, workingPath: string[]) {
+  if (path.startsWith('/')) {
+    workingPath.splice(0, workingPath.length);
+  }
+
+  const pathArr = path.split('/');
+
+  if (path.startsWith('/')) {
+    pathArr.shift();
+  }
+  if (path.endsWith('/')) {
+    pathArr.pop();
+  }
+
+  while (pathArr.length > 0) {
+    const pathSegment = pathArr.shift() as string;
+
+    if (pathSegment === '.') {
+      continue;
+    } else if (pathSegment === '..') {
+      workingPath.pop();
+    } else {
+      workingPath.push(pathSegment);
+    }
+  }
+
+  return workingPath;
+}
