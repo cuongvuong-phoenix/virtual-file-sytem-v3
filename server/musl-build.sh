e98c7e959c56f7d268c67b4c059afb8a18ce1372
Cuong Vuong Chi <cuongvuong.phoenix@gmail.com>
2022-03-24 22:06:15 +0700

    refactor(server/deploy): migrate to new *Docker* build to avoid redundant multi-stage images

---
+++ server/musl-build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run \
+    -v cargo-cache:/root/.cargo/registry \
+    -v "$PWD:/volume" \
+    -e SQLX_OFFLINE=true \
+    --rm -it clux/muslrust sh -c "cargo build --release && chown -R $(id -u):$(id -g) ./target"
